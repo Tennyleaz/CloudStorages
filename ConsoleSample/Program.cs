8a109a55e00e5d44258ae363ea6e43e0f2b754e9
Tennyleaz <tennyleaz@gmail.com>
2020-12-24 16:55:43 +0800

    更新範例 App。

--- ConsoleSample/Program.cs
+++ ConsoleSample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using CloudStorages;
 using CloudStorages.DropBox;
@@ -28,8 +29,17 @@ namespace ConsoleSample
 
         private static async Task Test()
         {
-            string apiKey = File.ReadAllText(@"D:\Test Dir\CloudStorages\dbkey.txt");
+            string apiKey;
             string redirectUrl = @"http://localhost:51001/";
+            try
+            {
+                apiKey = File.ReadAllText(@"D:\Test Dir\CloudStorages\dbkey.txt");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Cannot read api key: " + ex.Message);
+                return;
+            }
 
             ICloudStorageClient dropboxClent = new DropBoxStorage(apiKey, redirectUrl);
             dropboxClent.SaveAccessTokenDelegate = SaveAccessToken;
@@ -58,8 +68,48 @@ namespace ConsoleSample
                 Console.WriteLine("Login failed, reason=" + result.Message);
                 return;
             }
-            else
-                Console.WriteLine("Login success, account=" + accountInfo.userEmail);
+            
+            Console.WriteLine("Login success, account=" + accountInfo.userEmail);
+            IFormatProvider formatter = new CloudStorages.Utility.FileSizeFormatProvider();
+            Console.WriteLine(string.Format(formatter, "Account space: {0:fs} / {1:fs}", accountInfo.usedSpace, accountInfo.totalSpace));
+
+            string folderId, folderName = "/test/folder1";
+            (result, folderId) = await dropboxClent.CreateFolderAsync(folderName);
+            if (!result.Success)
+            {
+                Console.WriteLine("Create folder failed, reason=" + result.Message);
+                return;
+            }
+            Console.WriteLine($"Created folder {folderName}, id={folderId}");
+
+            string localFile = "test.txt";
+            File.WriteAllText(localFile, "I am a test file.");
+            CancellationTokenSource cts = new CancellationTokenSource();
+            result = await dropboxClent.UploadFileToFolderByPathAsync(localFile, cts.Token, folderName);
+            if (!result.Success)
+            {
+                Console.WriteLine("Upload file failed, reason=" + result.Message);
+                return;
+            }
+            Console.WriteLine($"Uploaded file {localFile}");
+            File.Delete(localFile);
+
+            string cloudFile = folderName + "/" + localFile;
+            result = await dropboxClent.DownloadFileByPathAsync(cloudFile, localFile, cts.Token);
+            if (!result.Success)
+            {
+                Console.WriteLine("Download file failed, reason=" + result.Message);
+                return;
+            }
+            Console.WriteLine($"Downloaded file {localFile}");
+
+            result = await dropboxClent.DeleteFileByPathAsync(cloudFile);
+            if (!result.Success)
+            {
+                Console.WriteLine("Delete file failed, reason=" + result.Message);
+                return;
+            }
+            Console.WriteLine($"Deleted file {localFile}");
         }
 
         #region Token save/load
